e99eac513ddf84af2d9cbd58916c49b0812a7f06
Endriw Braga <endriwbragalisboa@gmail.com>
2024-11-22 17:09:24 -0300

    Outro exercico de while.

--- curso_programacao/src/curso_programacao/Exercicio_while.java
+++ curso_programacao/src/curso_programacao/Exercicio_while.java
@@ -8,7 +8,7 @@ public class Exercicio_while {
 		int y = 0;
 		
 		while(x > 2) {
-			System.out.println(x);
+			//System.out.println(x);
 			
 			y = y + x;
 			x = x - 1;
@@ -16,7 +16,29 @@ public class Exercicio_while {
 		
 		// Oque deve ser imprimido
 		// y vai ficar com valor de 5
-		// x vai imprimir 4 3 2, e a condição vai ser falsa e vai encerrar o loop.
+		// x vai imprimir 5 4 3, e a condição vai ser falsa e vai encerrar o loop.
+		
+		
+		int a = 2;
+		int b = 0;
+		
+		while (a < 60) {
+			System.out.println(a);
+			System.out.println(b);
+
+			a = a * 2;
+			b = b + 10;
+		}
+		
+		// O a vai começar imprimindo 2 na tela, e vai imprimir 2, 4, 8, 16, 32 e nao vai imprimir o 
+		// 64 por que vai ser falsa a condição do while
+		
+		// O b começa imprimindo o 0,  e vai 10 20 30 40 e para por que a condição do a é falsa.
+		
+		
+		
+		
+		
 	}
 
 }
